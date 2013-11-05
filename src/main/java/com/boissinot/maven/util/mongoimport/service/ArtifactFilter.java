00f9de9bd95a8fdd3c5da4ccd0b90e5cea7b77e1
Gregory Boissinot <gregory.boissinot@gmail.com>
2013-11-05 18:08:21 +0100

    Specify as a  Spring Integ. Filter

--- src/main/java/com/boissinot/maven/util/mongoimport/service/ArtifactFilter.java
+++ src/main/java/com/boissinot/maven/util/mongoimport/service/ArtifactFilter.java
@@ -1,12 +1,15 @@
 package com.boissinot.maven.util.mongoimport.service;
 
 import com.boissinot.maven.util.mongoimport.domain.ArtifactObj;
+import org.springframework.integration.annotation.Filter;
 
 /**
  * @author Gregory Boissinot
  */
 public class ArtifactFilter {
 
+    @Filter
+    @SuppressWarnings("unused")
     public boolean isValidArtifact(ArtifactObj artifactObj) {
         return true;
     }
