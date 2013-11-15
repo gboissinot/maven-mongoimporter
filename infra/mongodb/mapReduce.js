c4127012bcbe0256b4bc27276293a5aa34af7f22
Gregory Boissinot <gregory.boissinot@gmail.com>
2013-11-15 18:00:10 +0100

    New version wiithout alpha

--- infra/mongodb/mapReduce.js
+++ infra/mongodb/mapReduce.js
@@ -7,68 +7,92 @@ var map_function_version = function () {
     };
     var value = {
         version: this.version,
-        isReferenceVersion: this.isReferenceVersion
-
+        isForcedVersion: this.isForcedVersion
     };
     emit(key, value);
 }
 
-var reduce_function_version = function(key, values) {
+var reduce_function_version = function (key, values) {
 
     var maxVersionDoc;
-    var curMaxVersionVal = "";
+    var curMaxVersionValTab = [];
+    var BreakException = {};
+    try {
+        values.forEach(
+            function (curObj) {
 
-    values.forEach(
-        function(curObj) {
+                //-- Get current version from values element
+                var curVersionVal = curObj.version;
 
-            //-- Get current version from values element
-            var curVersionVal = curObj.version;
+                //-- Case of isForcedVersion version
+                if (curObj.isForcedVersion) {
+                    maxVersionDoc = curObj;
+                    throw BreakException;
+                }
 
-            //-- Case of isReference version
-            if (curObj.isReferenceVersion) {
-                maxVersionDoc = curObj;
-                return;
-            }
+                //TODO MANAGE ALPHA, BETA, ... versions
+                var isContinue = true;
+                var curVersionValTab = curVersionVal.split(".");
+                var k = 0;
+                while (isContinue) {
+                    k++;
 
-            //-- Compute a new temp version value with threee members x.y.z
-            var initialTemporaryVersionTab = curVersionVal.split(".");
-            var targetTemporaryVersionTab = []
-            while (targetTemporaryVersionTab.length != 3) {
-                if (initialTemporaryVersionTab.length != 0) {
-                    targetTemporaryVersionTab.push(initialTemporaryVersionTab.shift());
-                } else {
-                    targetTemporaryVersionTab.push("0");
+                    if (curMaxVersionValTab.length < k) {
+                        isContinue = false;
+                        curMaxVersionValTab = curVersionValTab;
+                        maxVersionDoc = curObj;
+                    }
+
+                    else if (curVersionValTab.length < k) {
+                        isContinue = false;
+                    }
+
+                    else {
+                        var curMaxVersionValTabElt = parseInt(curMaxVersionValTab[k - 1]);
+                        var curVersionValTabElt = parseInt(curVersionValTab[k - 1]);
+                        if (curVersionValTabElt > curMaxVersionValTabElt) {
+                            isContinue = false;
+                            curMaxVersionValTab = curVersionValTab;
+                            maxVersionDoc = curObj;
+                        } else if (curVersionValTabElt < curMaxVersionValTabElt) {
+                            isContinue = false;
+                        }
+                    }
                 }
             }
-            var newCurVersionWithAtLeast3Elts = targetTemporaryVersionTab.join(".");
+        );
 
-            var curMaxVersion2Compare = curMaxVersionVal;
-            //Tip: For version ending with '-', we put a '0' to previous maxVersion
-            if (newCurVersionWithAtLeast3Elts.indexOf("-") != -1) {
-                curMaxVersion2Compare = curMaxVersion2Compare + ".0";
-            }
+        return  maxVersionDoc;
 
-            //Comparison: Use String comparison
-            if (newCurVersionWithAtLeast3Elts > curMaxVersion2Compare) {
-                curMaxVersionVal = newCurVersionWithAtLeast3Elts;
-                maxVersionDoc = curObj;
-            }
+    } catch (e) {
+        if (e === BreakException) {
+            return maxVersionDoc;
         }
-    );
 
-    return maxVersionDoc;
+        throw e;
+    }
 
 }
 
+var reduce_function_deprecated = function (key, values) {
 
-var reduce_function_deprecated = function(key, values) {
-
-    var maxVersionValue = reduce_function_version(key, values);
-    var idxElemToRemove = values.indexOf(maxVersionValue);
+    var maxVersionDoc = reduce_function_version(key, values);
+    var idxElemToRemove = values.indexOf(maxVersionDoc);
 
     //- final deprecated versions array of one element
     values.splice(idxElemToRemove, 1);
 
-    return values.join("*");
+    var deprecatedVersionTab = [];
+    values.forEach(
+        function (value) {
+            deprecatedVersionTab.push(value.version);
+        }
+    );
+
+    var newDoc = {
+        version: deprecatedVersionTab.join(" ")
+    };
+
+    return newDoc;
 }
 
